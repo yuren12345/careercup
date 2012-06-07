5fe7b1d7076a706399f519e28a3d152a9e573955
yuren12345 <user@server>
2012-06-06 21:25:27 -0700

    career 8.5

---
+++ careercup8.5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+void printParatheses(unsigned num, std::string *existing = NULL) {
+  std::string *temp;
+  if (existing != NULL)
+    temp = existing;
+  else
+    temp = new std::string();
+  
+  if (num == 0) {
+    std::cout << *temp << ", ";
+  } else {
+    unsigned left = 0;
+    for (int i = num - 1; i >= 0; --i) {
+      left++;
+      for (unsigned j = 0; j < left; ++j)
+        *temp += "(";
+      for (unsigned j = 0; j < left; ++j)
+        *temp += ")";        
+      printParatheses(i, temp);
+      
+      *temp = temp->substr(0, temp->size() - 2 * left);
+    }
+  }
+}
+
+int main() {
+  unsigned num;
+  std::cout << "Enter the pairs of paratheses you want:" << std::endl;
+  std::cin >> num;
+  
+  printParatheses(num);
+  std::cout << std::endl;
+  
+  return 0;
+}
