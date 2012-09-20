636524149407d38087b02399cf9792bf0c370218
yuren12345 <user@server>
2012-09-20 11:38:10 -0700

    careercup 1.2

---
+++ careercup1.2.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <cstring>
+
+void reverseStr(char *a) {
+  unsigned len = strlen(a);
+  
+  if (len > 1) {
+    unsigned mid = len / 2;
+    for (int i = 0; i < mid; ++i) {
+      char temp = a[i];
+      a[i] = a[len - 1 - i];
+      a[len - 1 - i] = temp;
+    }
+  }
+}
+
+
+int main(void) {
+  std::cout << "Input the string: " << std::endl;
+
+  std::string temp;
+  std::cin >> temp;
+  
+  char *ctemp = new char[temp.size() + 1];
+  strcpy(ctemp, temp.c_str());
+  reverseStr(ctemp);
+  
+  std::cout << "Reversed string is: " << std::endl;
+  printf("%s\n", ctemp);
+  
+  delete ctemp;
+  return 0;
+}
